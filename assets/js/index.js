8ce6e0c993fac66cd69a47ab2f526b039984bc32
eduardoanconamateus <dudle29@live.com>
2020-04-02 23:17:04 -0300

    modificações

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let params = coDesExtract()
-	let value = params['key']
-    let value2 = document.querySelector('.batatinha')
-  	value2.href = "projeto.html?cat="+value+"&pro={{@key}}"
-  	value2.src  = "assets/img/{{key}}.jpg"
-  	console.log(value2)
-  	let db = coDesConnect('https://codes-luiz.firebaseio.com/portfolio/portfolio')
+  	let db = coDesConnect('https://codes-luiz.firebaseio.com/')
 
-  	db.download('/', function(data)) {
-  		context = data
+  	db.download('/', function(data) {
+      context = data
   		console.log(context)
-  		coDesReplace('.ul-categoria', context )
-  	}
-}
+  		coDesReplace('.ul-categoria-menu', context )
+  	})
+})
