e055511adebf05a219bd73d52778260b58662068
Gabriel Tkacz <55806524+gtkacz@users.noreply.github.com>
2020-06-24 18:52:59 -0300

    final

--- assets/js/projeto.js
+++ assets/js/projeto.js
@@ -4,22 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
   let proj = params['pro']
   let db = coDesConnect('https://codes-luiz.firebaseio.com/')
 
-  //let imag = document.querySelector('.batato')
-  //imag.src = "assets/img/"+proj+".jpg"
   
   db.download('/', function(data) {
-    context = data
-    //coDesReplace('.nav-cat-sup', context)
-
+    context = data.portfolio
+    coDesReplace('.ul-categoria', context)
     context = data['portfolio']['portfolio'][value]['projetos'][proj]
     console.log(context)
-    /*coDesReplace('.k1', context)
-    coDesReplace('.k2', context)
-    coDesReplace('.k3', context)
-    coDesReplace('.k4', context)
-    context = data['portfolio']['portfolio'][value]['projetos'][proj]
-    console.log(context)
-    coDesReplace('.k5',context)
-    coDesReplace('.k6', context)*/
+    coDesReplace('.maca', context)
+    coDesReplace('.uva', context)
+    coDesReplace('.poloni', context)
   })
 })
